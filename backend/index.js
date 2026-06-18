22134266ce03cbf7cf0b65a13cac7baf1d42ae83
Akshayaa <akshayaasri1311@gmail.com>
2026-06-18 18:22:10 +0530

    fix: smtp port 465

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,9 @@ const nodemailer = require("nodemailer");
 let otpStore = {};
 
 const transporter = nodemailer.createTransport({
-   service: "gmail",
+   host: "smtp.gmail.com",
+  port: 465,
+  secure: true,  // true for port 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
